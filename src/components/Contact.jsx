e3164b9efb6d174574095af2bdd7885107f157a4
Victor Queiroga <victor.carvalho@green4t.com>
2023-03-13 20:32:25 -0300

    Feat: add placeholder page to /contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+export const Contact = () => {
+    return (
+        <div>
+            <hr className="bg-black h-[2px]"/>
+            <div className="flex justify-center">
+                <div className="h-[1200px]"></div>
+                <button className="btn">as</button>
+            </div>
+        </div>
+    )
+}
