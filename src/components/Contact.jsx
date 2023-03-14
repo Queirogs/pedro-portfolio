ea258ba868860826f5761775119790032f3f3039
Victor Queiroga <victor.carvalho@green4t.com>
2023-03-14 13:44:42 -0300

    Feat: add first page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,27 @@
 export const Contact = () => {
     return (
         <div>
-            <hr className="bg-black h-[2px]"/>
-            <div className="flex justify-center">
-                <div className="h-[1200px]"></div>
-                <button className="btn">as</button>
+            <div className="flex justify-between">
+                <div className="bg-[#FFE600] grow-1 w-[50%] aspect-video items-center flex justify-center border-black border-2">
+                    <label className="font-albert font-extrabold text-[100px]">ESTRATÉG</label>
+                </div>
+                <div className="grow-1 border-y-black border-r-black border-2 flex justify-center w-[50%] items-center">
+                    <label className="font-albert font-extrabold text-[100px]">ESTRATÉG</label>
+                </div>
+            </div>
+
+            <div className="h-[560px] border-2 border-b-black border-x-black px-[80px] relative">
+                <div className="absolute bottom-0">
+                    <label class="text-[100px] font-albert font-extrabold">ESTRATÉG</label>
+                    <label className="text-[30px] font-albert font-regular">22 / Caros amig</label>
+                </div>
+            </div>
+
+            <div className="h-[560px] border-2 border-b-black border-x-black px-[80px] relative">
+                <div className="absolute bottom-0">
+                    <label class="text-[100px] font-albert font-extrabold">ESTRATÉG</label>
+                    <label className="text-[30px] font-albert font-regular">22 / Caros amig</label>
+                </div>
             </div>
         </div>
     )
