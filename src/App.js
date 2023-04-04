6968caad39f2e6b50336bb50c7df69970e5677ec
Victor Queiroga <victor.carvalho@green4t.com>
2023-04-04 18:10:11 -0300

    Feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
-import { Contact } from './components/Contact';
+import { Design } from './pages/Design';
 import { UpButton } from './components/UpButton';
 import { Footer } from './components/Footer';
+import { About } from './pages/About';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div class="">
       <Header />
-      <Contact />
+
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component={Design} />
+        <Route path="/design" element={<Design />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
       <UpButton />
       <Footer />
     </div>
