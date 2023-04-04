869d26a637ebf011131ce2ef8cf667183e98d815
Victor Queiroga <victor.carvalho@green4t.com>
2023-04-04 18:11:07 -0300

    Feat: rework on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,32 @@
 import Rock from '../img/rock.png'
+import Arrow from '../img/downarrow.png'
+import Spin from '../img/designrocks.svg'
 
 export const Footer = () => {
     return (
-        <div className="border-x-black border-b-black border-2">
-            <div className="flex justify-between mt-[50px] mx-[80px]">
-                <label className="w-[50%] text-[20px] font-albert font-extrabold ">
-                    Ainda assim, existem dúvidas a respeito de como o surgimento do comércio virtual auxilia a preparação
-                    e a composição dos conhecimentos estratégicos para atingir a excelência. Ainda assim, existem dúvidas
-                    a respeito de como o surgimento do comércio virtual auxilia a preparação e a composição dos
-                    conhecimentoestratégicos para atingir a excelência.Ainda assim, existem dúvidas a respeito de como o
-                    surgimento do comércio virtual
-                </label>
+        <div className="">
+            <div className="flex justify-between mt-[100px] mx-[80px]">
+                <div>
+                    <label className="text-[20px] font-albert font-bolds">Oie! :) <br /></label>
+                    <label className="w-[50%] text-[20px] font-albert font-regular ">
+                        Que tal um café? <br />
+                        contactpedroalighieri@gmail.com
+                    </label>
+                    <div className="mt-[48px]">
+                        <div className="flex">
+                            <img className="h-[38px] aspect-square mr-[16px]" src={Arrow} alt="" />
+                            <label className="font-albert font-bold text-[35px] self-start">INSTAGRAM</label>
+                        </div>
+                        <div className="flex">
+                            <img className="h-[38px] aspect-square mr-[16px]" src={Arrow} alt="" />
+                            <label className="font-albert font-bold text-[35px] self-center">BEHANCE</label>
+                        </div>
+                    </div>
+
+                    <label className="font-albert font-bold text-[18px] mt-96">2023 - Pedro Alighieri</label>
+                </div>
                 <img className="w-[373px]" src={Rock} alt="" />
+                <img className="right-[5%] bottom-[-1350px] absolute animate-spin w-[326px] aspect-square" src={Spin} alt="" /> 
             </div>
 
         </div>
